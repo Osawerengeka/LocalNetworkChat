6ae1842d5f52a65a54256ad527ff935e75042f73
George <mr.georgegevork@gmai.com>
2020-07-05 21:08:05 +0300

    Revert "app now can receive and send messages(without Files and ChatHistory)"
    This reverts commit 693cf61a99376dd27631947a995309e7ef25d1f2.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,102 +13,25 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Diagnostics;
-using System.Net;
-using System.Collections.Specialized;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Net.Sockets;
-using System.Threading;
-using System.Configuration;
-using System.Collections.ObjectModel;
-using System.IO;
 
 namespace LocalNetworkChat
 {
 
     public partial class MainWindow : Window
     {
-        static ObservableCollection<string> a = new ObservableCollection<string>();
-        static string remoteAddress; 
-        static int remotePort;
-        static int localPort; 
-        /*public string cmd(string command)
-        {
-            Process p = new Process();
-            p.StartInfo.UseShellExecute = false;
-            p.StartInfo.RedirectStandardOutput = true;
-            p.StartInfo.FileName = "C:\\C.bat";
-            p.Start();
-            string output = p.StandardOutput.ReadToEnd();
-            return output;
-        }*/
+        string command;
         public MainWindow()
         {
+
             InitializeComponent();
+            Process cmd = new Process();
+            cmd.StartInfo = new ProcessStartInfo(@"cmd.exe");
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.Start();
+            cmd.StandardInput.WriteLine(command);
+
         }
 
-        private void OpenChat(object sender, RoutedEventArgs e)
-        {
-            a.Clear();
-            localPort = Int32.Parse(Vport.Text);
-            remotePort =Int32.Parse(Iport.Text);
-            remoteAddress = IP.Text;
-           
-            //здесь потом надо загрузить старый чат
-            Thread receiveThread = new Thread(new ThreadStart(ReceiveMessage));
-            receiveThread.Start();
-        }
-        private void SendMessage(object sender, RoutedEventArgs e)
-        {
-            UdpClient send = new UdpClient(); // создаем UdpClient для отправки сообщений
-            try
-            {
-                    string message = msg.Text; // сообщение для отправки
-                    byte[] data = Encoding.Unicode.GetBytes(message);
-                    send.Send(data, data.Length, remoteAddress, remotePort); // отправка
-                    string writePath = @"\" + remoteAddress + @".txt";
-                    using (StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.Default))
-                        sw.WriteLine(message);
-                    a.Add(message);
-                    ListofMessages.ItemsSource = a;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            finally
-            {
-                send.Close();
-            }
-        }
-
-        private  void ReceiveMessage()
-        {
-            UdpClient receiver = new UdpClient(localPort); // UdpClient для получения данных
-            IPEndPoint remoteIp = null; // адрес входящего подключения
-            try
-            {
-                while (true)
-                {
-                    byte[] data = receiver.Receive(ref remoteIp); // получаем данные
-                    string message = Encoding.Unicode.GetString(data);
-                    string writePath = @"\" + remoteAddress + @".txt";
-                    using (StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.Default))
-                        sw.WriteLine(message);
-                    a.Add(message);                
-                    ListofMessages.ItemsSource = a;
-                    //Console.WriteLine("Собеседник: {0}", message);
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            finally
-            {
-                receiver.Close();
-            }
-        }
     }
 }
